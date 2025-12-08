fe45432c539a6608e1993e994e380aca4e432501
Anusha Goel <nushie@Anushas-MacBook-Air-2.local>
2025-12-08 08:30:56 -0500

    added the initial game files to github

---
+++ src/Options.java
@@ -0,0 +1,20 @@
+import javax.swing.JButton;
+
+public class Options {
+    public static JButton cute = new JButton();
+    public static JButton alt = new JButton();
+    public static JButton cuteDress = new JButton();
+    public static JButton cuteTop = new JButton();
+    public static JButton cuteJeans = new JButton();
+    public static JButton cuteSkirt = new JButton();
+    public static JButton cuteHair = new JButton();
+    public static JButton cuteJewelry = new JButton(); 
+    public static JButton altDress = new JButton();
+    public static JButton altTop = new JButton();
+    public static JButton altJBottoms = new JButton();
+    public static JButton altSkirt = new JButton();
+    public static JButton altHair = new JButton();
+    public static JButton altMakeup = new JButton(); 
+
+}
+    
---
+++ src/StyleSavvy.java
@@ -0,0 +1,51 @@
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Font;
+
+public class StyleSavvy extends JFrame {
+    public StyleSavvy() {
+        // title window
+        super("Style Savvy");
+
+        // Setting the default close operation (exit when the window is closed)
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Making a panel(basic attributes and customization)
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        panel.setPreferredSize(new Dimension(1300, 800));
+        panel.setBackground(new Color(255, 217, 231));
+    
+        // making both buttons
+         Font buttonFont = new Font("Serif", Font.PLAIN, 30);
+
+        JButton button1 = new JButton("Cute!");
+        button1.setBounds(150, 500, 400, 200);
+        button1.setFont(buttonFont);
+        button1.addActionListener(new ActionListenerVV(Options.cute));
+        panel.add(button1);
+
+        JButton button2 = new JButton("Alternative!");
+        button2.setBounds(750, 500, 400, 200);
+        button2.setFont(buttonFont);
+        button2.addActionListener(new ActionListenerVV(Options.alt));
+        panel.add(button2);
+
+        // making a label
+        JLabel label = new JLabel((new ImageIcon("resources/test.png")));
+        label.setBounds(50, 300, 100, 100);
+        panel.add(label);
+
+        // adding the panel in
+        add(panel);
+
+        // Packing the frame + making it visible
+        pack();
+        setVisible(true);
+    }
+}
