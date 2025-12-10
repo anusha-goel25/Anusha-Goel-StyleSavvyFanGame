53a7d26f9d1f8281d5339a05f232e8622d639352
Anusha Goel <nushie@anushas-air-2.myfiosgateway.com>
2025-12-09 20:50:55 -0500

    Made the file tree system, but have not fully tested if it works yet

---
+++ src/ScreenContainer.java
@@ -0,0 +1,102 @@
+import javax.swing.JPanel;
+
+public class ScreenContainer {
+    static JPanel screenOriginal = new JPanel();
+    static JPanel screenCute = new JPanel();
+    static JPanel screenCuteDress = new JPanel();
+    static JPanel screenAlt = new JPanel();
+    static JPanel screenAltDress = new JPanel();
+    static JPanel screenAltTop = new JPanel();
+    static JPanel screenCuteTop = new JPanel();
+    static JPanel screenAltBottoms = new JPanel();
+    static JPanel screenAltSkirt = new JPanel();
+    static JPanel screenCuteJeans = new JPanel();
+    static JPanel screenCuteSkirt = new JPanel();
+    static JPanel screenCuteHair = new JPanel();
+    static JPanel screenCuteHairband = new JPanel();
+    static JPanel screenCuteHairclip = new JPanel();
+    static JPanel screenCuteJewelry = new JPanel();
+    static JPanel screenCuteJewelryEarrings = new JPanel();
+    static JPanel screenCuteJewelryNecklace = new JPanel();
+    static JPanel screenAltHair = new JPanel();
+    static JPanel screenAltBoldHair = new JPanel();
+    static JPanel screenAltNaturalHair = new JPanel();
+    static JPanel screenAltMakeup = new JPanel();
+    static JPanel screenAltHeavyMakeup = new JPanel();
+    static JPanel screenAltLightMakeup = new JPanel();
+
+    static Screen Original = new Screen(screenOriginal);
+
+    //this is taken from the morsecode activity and tweaked a bit(made setup public so that I can call it in my main file)
+    public static void setup() {
+        if (Screen.isSetup) return;
+        Screen.isSetup = true;
+
+        //cute path
+        Original.left = new Screen(screenCute);
+        Original.left.left = new Screen(screenCuteTop);
+        Original.left.right = new Screen(screenCuteDress);
+        Original.left.right.left = new Screen(screenCuteHair);
+        Original.left.right.left.left = new Screen(screenCuteHairclip);
+        Original.left.right.left.right = new Screen(screenCuteHairband);
+        Original.left.right.right = new Screen(screenCuteJewelry);
+        Original.left.right.right.left = new Screen(screenCuteJewelryEarrings);
+        Original.left.right.right.right = new Screen(screenCuteJewelryNecklace);
+        Original.left.left.left = new Screen(screenCuteJeans);
+        Original.left.left.left.left = new Screen(screenCuteHair);
+        Original.left.left.left.left.left = new Screen(screenCuteHairclip);
+        Original.left.left.left.left.right = new Screen(screenCuteHairband);
+        Original.left.left.left.right = new Screen(screenCuteJewelry);
+        Original.left.left.left.right.left = new Screen(screenCuteJewelryEarrings);
+        Original.left.left.left.right.right = new Screen(screenCuteJewelryNecklace);
+        Original.left.left.right = new Screen(screenCuteSkirt);
+        Original.left.left.right.left = new Screen(screenCuteHair);
+        Original.left.left.right.left.left = new Screen(screenCuteHairclip);
+        Original.left.left.right.left.right = new Screen(screenCuteHairband);
+        Original.left.left.right.right = new Screen(screenCuteJewelry);
+        Original.left.left.right.right.left = new Screen(screenCuteJewelryEarrings);
+        Original.left.left.right.right.right = new Screen(screenCuteJewelryNecklace);
+
+        //alt path
+        Original.right = new Screen(screenAlt);
+        Original.right.left = new Screen(screenAltTop);
+        Original.right.right = new Screen(screenAltDress);
+        Original.right.right.left = new Screen(screenAltHair);
+        Original.right.right.left.left = new Screen(screenAltBoldHair);
+        Original.right.right.left.right = new Screen(screenAltNaturalHair);
+        Original.right.right.right = new Screen(screenAltMakeup);
+        Original.right.right.right.left = new Screen(screenAltHeavyMakeup);
+        Original.right.right.right.right = new Screen(screenAltLightMakeup);
+        Original.right.left.left = new Screen(screenAltBottoms);
+        Original.right.left.left.left = new Screen(screenAltHair);
+        Original.right.left.left.left.left = new Screen(screenAltBoldHair);
+        Original.right.left.left.left.right = new Screen(screenAltNaturalHair);
+        Original.right.left.left.right = new Screen(screenAltMakeup);
+        Original.right.left.left.right.left = new Screen(screenAltHeavyMakeup);
+        Original.right.left.left.right.right = new Screen(screenAltLightMakeup);
+        Original.right.left.right = new Screen(screenAltSkirt);
+        Original.right.left.right.left = new Screen(screenAltHair);
+        Original.right.left.right.left.left = new Screen(screenAltBoldHair);
+        Original.right.left.right.left.right = new Screen(screenAltNaturalHair);
+        Original.right.left.right.right = new Screen(screenAltMakeup);
+        Original.right.left.right.right.left = new Screen(screenAltHeavyMakeup);
+        Original.right.left.right.right.right = new Screen(screenAltLightMakeup);
+        
+    }
+    //This stuff is taken from the morsecode exercise we did
+    public static class Screen {
+        Screen left;
+        Screen right;
+        JPanel panel;
+        private static boolean isSetup = false;
+        static Screen start;
+
+        Screen(JPanel panel) {
+            this.panel = panel;
+            if (start == null){
+                start = this;
+            }
+        }
+
+    }
+}
--- src/StyleSavvy.java
+++ src/StyleSavvy.java
@@ -11,18 +11,22 @@ public class StyleSavvy extends JFrame {
     public StyleSavvy() {
         // title window
         super("Style Savvy");
-
+        makeOpeningPanel();
         // Setting the default close operation (exit when the window is closed)
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
+
+    void makeOpeningPanel() {
+        
         // Making a panel(basic attributes and customization)
         JPanel panel = new JPanel();
         panel.setLayout(null);
         panel.setPreferredSize(new Dimension(1300, 800));
         panel.setBackground(new Color(255, 217, 231));
-    
+
         // making both buttons
-         Font buttonFont = new Font("Serif", Font.PLAIN, 30);
+        Font buttonFont = new Font("Serif", Font.PLAIN, 30);
 
         JButton button1 = new JButton("Cute!");
         button1.setBounds(150, 500, 400, 200);
